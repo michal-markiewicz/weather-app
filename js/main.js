c662f970b96eea8488fd23bf681b03b8da417fec
michal247 <michalmarkiewicz0953@gmail.com>
2022-04-26 11:03:20 +0200

    printWeather

--- js/main.js
+++ js/main.js
@@ -14,13 +14,12 @@ function geocoding (cityName)
         .then((responseBody) => {
             const lat = responseBody[0].lat;
             const lon = responseBody[0].lon;
-
             getWeather(lat, lon);
         })
     })
 }
 
-geocoding("Warsaw");
+geocoding("Paris");
 
 function getWeather (lat, lon)
 {
@@ -35,8 +34,50 @@ function getWeather (lat, lon)
 
 function printWeather (data)
 {
+    console.log(data);
     const currentDateHtml = document.getElementById('current-date');
+
+    const currentDateTimestampMs = data.current.dt * 1000;
+    const currentDate = new Date(currentDateTimestampMs);
+    currentDateHtml.textContent = currentDate.toLocaleString("en-US", {"dateStyle" : "full"});
+
+    const currentWeatherHtml = document.getElementById('current-weather');
+
+    currentWeatherHtml.textContent = `Current weather: ${Math.round(data.current.temp)} celsius, ${data.current.weather[0].main.toLowerCase()}`;
+
+    const weeklyWeatherHtml = document.getElementById('weekly-weather-table');
+    weeklyWeatherHtml.innerHTML = `
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Temperature</th>
+        <th>Conditions</th>
+        <th class="mobile-hidden">Wind speed</th>
+        <th class="mobile-hidden">Wind direction</th>
+    </tr>
+    </thead>
+    <tbody>
+    ${data.daily.map((day) => {
+        return `
+        <tr>
+            <td>${day.dt}</td>
+            <td>${Math.round(day.temp.day)} celsius</td>
+            <td>${day.weather[0].main.toLowerCase()}</td>
+            <td class="mobile-hidden">${day.wind_speed}</td>
+            <td class="mobile-hidden">${day.wind_deg}</td>
+        </tr>
+    `
+    }).join('')}
+    </tbody>
+`;
 }
+
+
+
+
+
+
+
 
 
 
